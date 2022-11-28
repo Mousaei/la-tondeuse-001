beb3e33ff272ac2565990df03cb97df97c0dcf02
Mousaei <vahidmfr@yahoo.fr>
2022-11-28 09:33:46 +0100

    Mower Program Code

--- src/main/java/latendeuse001/LaunchMower.java
+++ src/main/java/latendeuse001/LaunchMower.java
@@ -3,8 +3,10 @@
  */
 package latendeuse001;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
@@ -37,13 +39,27 @@ public class LaunchMower {
 		
 		properties.setProperty("testFileName" , "TestResource");
 		
-		properties.setProperty("testResultFileName" , "/Users/vahidmousaei/eclipse-workspace/latendeuse001/src/main/resources/ResultTestResource");
+		//properties.setProperty("testResultFileName" , "/Users/vahidmousaei/eclipse-workspace/latendeuse001/src/main/resources/ResultTestResource");
 		
-		
-		PrepareAndRun runIt = new PrepareAndRun(properties.getProperty("testFileName") , properties.getProperty("testResultFileName"));
+		java.net.URL url = ClassLoader.getSystemResource("prod.properties");
+
+        try  {
+            properties.load(url.openStream());
+        } catch (FileNotFoundException fie) {
+            fie.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(properties.getProperty("testFileName"));
+        System.out.println(properties.getProperty("testResultFileName"));
+        PrepareAndRun runIt = new PrepareAndRun(properties.getProperty("testFileName") , properties.getProperty("testResultFileName"));
 		
 		runIt.toString();
 		
-	}
-
+        Set<String> keys = properties.stringPropertyNames();
+        for (String key : keys) {
+            System.out.println(key + " - " + properties.getProperty(key));
+        }
+    }
 }
